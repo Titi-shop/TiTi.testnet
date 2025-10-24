03af11c85b0e212dfd87b6bc08cd4770409de406
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-10-24 23:50:40 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -62,24 +62,6 @@ export default function Navbar() {
           <Globe size={22} />
         </Link>
       </div>
-
-      {/* 🔹 Hàng dưới: Thanh điều hướng các trang */}
-      <nav className="flex justify-around border-t bg-gray-50 py-2">
-        {navItems.map(({ href, label, icon: Icon }) => {
-          const active = pathname === href;
-          return (
-            <Link
-              key={href}
-              href={href}
-              className={`flex flex-col items-center text-xs ${
-                active ? "text-indigo-600 font-semibold" : "text-gray-500 hover:text-black"
-              }`}
-            >
-              <Icon className={`w-5 h-5 mb-1 ${active ? "stroke-indigo-600" : ""}`} />
-              {label}
-            </Link>
-          );
-        })}
       </nav>
     </header>
   );
