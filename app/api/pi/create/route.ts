984bafec466cf565d1246f3f9559aa733aa6ce03
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-10-25 18:50:11 +0700

    Update route.ts

--- app/api/pi/create/route.ts
+++ app/api/pi/create/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request) {
     }
 
     const API_KEY = process.env.PI_API_KEY;
-const API_URL = process.env.PI_API_URL || "https://api.minepi.com/v2/payments";
+const API_URL = process.env.PI_API_URL || "https://api.minepi.com/v2/sandbox";
     if (!API_KEY) {
       console.error("❌ Thiếu PI_API_KEY trong biến môi trường");
       return NextResponse.json({ error: "Thiếu API key" }, { status: 500 });
