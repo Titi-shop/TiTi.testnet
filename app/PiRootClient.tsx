0e8b388d5ef34cac1741392636ed5c8176f1c3fc
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-10-31 20:41:55 +0700

    Create PiRootClient.tsx

---
+++ app/PiRootClient.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { AuthProvider } from "@/context/AuthContext";
+import { CartProvider } from "@/app/context/CartContext";
+import { LanguageProvider } from "@/app/context/LanguageContext";
+import Navbar from "@/components/Navbar";
+import BottomNav from "@/components/BottomNav";
+import PiProvider from "@/app/pi/PiProvider";
+import PiStatus from "@/components/PiStatus";
+
+export default function PiRootClient({ children }: { children: React.ReactNode }) {
+  return (
+    <LanguageProvider>
+      <AuthProvider>
+        <CartProvider>
+          <PiProvider />
+          <Navbar />
+          <main className="pt-20 px-3">
+            <PiStatus />
+            {children}
+          </main>
+          <BottomNav />
+        </CartProvider>
+      </AuthProvider>
+    </LanguageProvider>
+  );
+}
