d800a996c580c9673d38b9fdd15dc55797c9aa9b
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-03 23:11:29 +0700

    Update page.tsx

--- app/seller/page.tsx
+++ app/seller/page.tsx
@@ -160,9 +160,10 @@ export default function SellerDashboard() {
           <span className="mt-2 font-semibold block">
             💰 {translate("wallet") || "Ví Pi"}
           </span>
-         </Link>
-         </button>
-         </div>
+</Link>
+</button>
+</div>
+
     </main>
   );
 }
