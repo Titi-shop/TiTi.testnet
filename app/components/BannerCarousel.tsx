15adb48c4acb48f4b98c24577a4aff825b46a5ad
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-05 23:30:21 +0700

    Update BannerCarousel.tsx

--- app/components/BannerCarousel.tsx
+++ app/components/BannerCarousel.tsx
@@ -8,9 +8,21 @@ import "swiper/css/pagination";
 
 export default function BannerCarousel() {
   const [banners, setBanners] = useState([
-    { id: 1, image: "/banners/banner1.jpg", link: "#" },
-    { id: 2, image: "/banners/banner2.jpg", link: "#" },
-    { id: 3, image: "/banners/banner3.jpg", link: "#" },
+    {
+      id: 1,
+      image: "/banners/d506c80c-c548-41ce-b0e2-79dafa6d2de4.jfif",
+      link: "#",
+    },
+    {
+      id: 2,
+      image: "/banners/c60da310-1c35-4598-9ddb-e1457741a262.jfif",
+      link: "#",
+    },
+    {
+      id: 3,
+      image: "/banners/b42db293-7ba1-41a2-9bd1-7373ca643943.jfif",
+      link: "#",
+    },
   ]);
 
   return (
