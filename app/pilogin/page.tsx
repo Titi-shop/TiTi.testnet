fdef2946edd3f9c44dc8ab7b57258b23470f9b24
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-05 14:38:20 +0700

    Update page.tsx

--- app/pilogin/page.tsx
+++ app/pilogin/page.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
-import toast from "react-hot-toast";
-
 export default function PiLoginPage() {
   const router = useRouter();
   const { user, piReady, pilogin } = useAuth();
