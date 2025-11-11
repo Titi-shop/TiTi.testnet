ae3b3ebb4bde7739c21ba8075deddc74474cd1a7
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-12 02:27:23 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          500: '#f97316', // đảm bảo có màu bg-orange-500
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
