7e66d8ae4933df8898b404a6536192e2214cf48f
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-14 01:18:15 +0700

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useAuth } from "@/context/AuthContext";
-import { useLanguage } from "@/context/LanguageContext";
+import { useLanguage } from "../context/LanguageContext";
 import Link from "next/link";
 
 export default function ShopPage() {
