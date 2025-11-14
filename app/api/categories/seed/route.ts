3d3003d8dc6f77353e5df7a989c7f63013db68a7
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-14 19:48:25 +0700

    Update route.ts

--- app/api/categories/seed/route.ts
+++ app/api/categories/seed/route.ts
@@ -5,9 +5,9 @@ export async function GET() {
   const categories = [
     { id: 1, name: "Điện thoại & Laptop", icon: "https://muasam.titi.onl/banners/dienthoai.jpg" },
     { id: 2, name: "Thời trang Nam", icon: "https://placekitten.com/201/200" },
-    { id: 3, name: "Thời trang Nữ", icon: "https://placekitten.com/202/200" },
+    { id: 3, name: "Thời trang Nữ", icon: "/banners/Screenshot_2025-11-14-19-30-34-208_com.shopee.vn.jpg" },
     { id: 4, name: "Giày dép", icon: "/banners/Screenshot_2025-11-14-19-32-23-378_com.vuahanghieu.jpg" },
-    { id: 5, name: "Làm đẹp", icon: "https://placekitten.com/204/200" },
+    { id: 5, name: "Làm đẹp", icon: "public/banners/Screenshot_2025-11-14-19-34-37-576_com.shopee.vn.jpg" },
     { id: 6, name: "Nhà cửa & Đời sống", icon: "https://placekitten.com/205/200" },
     { id: 7, name: "Mẹ & Bé", icon: "https://placekitten.com/206/200" },
     { id: 8, name: "Thiết bị điện tử", icon: "https://placekitten.com/207/200" },
