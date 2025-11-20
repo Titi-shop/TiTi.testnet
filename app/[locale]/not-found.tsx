1ed4ed91150d9802aa4f62f32d4aee8159d1b7db
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-20 11:47:07 +0700

    Delete app/[locale]/not-found.tsx

--- app/[locale]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function LocaleNotFound() {
-  return (
-    <main className="p-6 text-center">
-      <h1 className="text-2xl font-bold">404 - Page not found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <a href="/" className="text-blue-600 underline">Go back home</a>
-    </main>
-  );
-}
