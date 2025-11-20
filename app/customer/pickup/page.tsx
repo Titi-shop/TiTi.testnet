7649836148128fdd958df2a511ea6f185ed15bbf
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-11-20 10:31:57 +0700

    Update page.tsx

--- app/customer/pickup/page.tsx
+++ app/customer/pickup/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useLanguage } from "../../context/LanguageContext";
+import { useTranslations } from 'next-intl';
 import { useAuth } from "@/context/AuthContext"; // 👉 import AuthContext
 
 export default function PickupOrdersPage() {
