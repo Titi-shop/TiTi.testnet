8fa0866d3652ca3cb5cfe7d0b522404fab30350b
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-12-03 00:49:15 +0700

    Update page.tsx

--- app/customer/review/page.tsx
+++ app/customer/review/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import "@/app/lib/i18n";
+import { useTranslationClient as useTranslation } from "@/app/lib/i18n/client";
 import { useAuth } from "@/context/AuthContext";
 
 interface OrderItem {
