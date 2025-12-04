b391d4a3b0daa715614a1b45b3f16181c23047c0
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-12-04 21:01:56 +0700

    Update route.ts

--- app/api/pi/verify/route.ts
+++ app/api/pi/verify/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import crypto from "crypto";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export interface PiUser {
   username: string;
