ac0040826518a69ab7a908d4250c057e8c98515a
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-12-08 02:16:02 +0700

    Update PiRootClient.tsx

--- app/PiRootClient.tsx
+++ app/PiRootClient.tsx
@@ -4,13 +4,11 @@ import { Toaster } from "react-hot-toast";
 import { CartProvider } from "@/app/context/CartContext";
 import Navbar from "@/components/Navbar";
 import BottomNav from "@/components/BottomNav";
-import PiProvider from "@/app/pi/PiProvider";
 import "@/app/lib/i18n";
 
 export default function PiRootClient({ children }: { children: React.ReactNode }) {
   return (
     <CartProvider>
-      <PiProvider />
       <Toaster position="top-center" reverseOrder={false} />
 
       <Navbar />
