c8789ca46cb1332e4eb999e82cd8c5bae179bff3
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-12-08 23:35:27 +0700

    Update PiRootClient.tsx

--- app/PiRootClient.tsx
+++ app/PiRootClient.tsx
@@ -5,27 +5,22 @@ import Navbar from "@/components/Navbar";
 import BottomNav from "@/components/BottomNav";
 
 import { CartProvider } from "@/app/context/CartContext";
-import { AuthProvider } from "@/context/AuthContext";
 
 import "@/app/lib/i18n";
 
 export default function PiRootClient({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <Toaster position="top-center" reverseOrder={false} />
+    <CartProvider>
+      <Toaster position="top-center" reverseOrder={false} />
 
-        {/* HEADER */}
-        <Navbar />
+      {/* HEADER */}
+      <Navbar />
 
-        {/* ⭐ ĐẨY NỘI DUNG XUỐNG DƯỚI NAVBAR - FULL WIDTH */}
-        <main className="pt-[72px] min-h-screen bg-white">
-          {children}
-        </main>
+      <main className="pt-[72px] min-h-screen bg-white">
+        {children}
+      </main>
 
-        {/* FOOTER */}
-        <BottomNav />
-      </CartProvider>
-    </AuthProvider>
+      <BottomNav />
+    </CartProvider>
   );
 }
