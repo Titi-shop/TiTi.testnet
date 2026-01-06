de1d265daccf094f227f4303d058887ce0190d43
Titi-shop <226829233+Titi-shop@users.noreply.github.com>
2026-01-06 23:49:31 +0700

    Update route.ts

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json([
-    { id: 1, name: "Điện thoại & Laptop", icon: "/banners/maytinh.jpg" },
+    { id: 1, name: "Điện thoại & Laptop", icon: "/banners/laptop.jpg" },
     { id: 2, name: "Thời trang Nam", icon: "/banners/thời trang nam.jpg" },
-    { id: 3, name: "Thời trang Nữ", icon: "/banners/thoitrangnu.jpg" },
-    { id: 4, name: "Giày dép", icon: "/banners/Screenshot_2025-11-14-19-32-23-378_com.vuahanghieu.jpg" },
-    { id: 5, name: "Làm đẹp", icon: "/banners/lamdep.jpg" },
+    { id: 3, name: "Thời trang Nữ", icon: "/banners/thời trang nữ.jpg" },
+    { id: 4, name: "Giày dép", icon: "/banners/giày dép.jpg" },
+    { id: 5, name: "Nước hoa & Mỹ ", icon: "/banners/nước hoa.jpg" },
     { id: 20, name: "Nội thất", icon: "/banners/noithat.jpg" },
     { id: 7, name: "Mẹ & Bé", icon: "/banners/mevabe.jpg" },
     { id: 8, name: "Thiết bị điện tử", icon: "/banners/dienthoai.jpg" },
@@ -17,8 +17,8 @@ export async function GET() {
     { id: 13, name: "Thú cưng", icon: "/banners/thucung.jpg" },
     { id: 14, name: "Điện máy", icon: "/banners/dienmay.jpg" },
     { id: 15, name: "Sách", icon: "/banners/sach.jpg" },
-    { id: 16, name: "Đồng hồ", icon: "/banners/dongho.jpg" },
+    { id: 16, name: "Đồng hồ", icon: "/banners/đông hồ .jpg" },
     { id: 19, name: "Đồ chơi", icon: "/banners/dochoi.jpg" },
-    { id: 18, name: "Máy ảnh & Flycam", icon: "/banners/flycam.jpg" },
+    { id: 18, name: "Máy ảnh & Flycam", icon: "/banners/máy ảnh.jpg" },
   ]);
 }
